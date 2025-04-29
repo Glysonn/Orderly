3fceac16fcaf8035f0415917e0b6ad680e36b0da
Glysonn <kauaglyson@gmail.com>
2025-04-29 00:11:45 -0300

    chore: restructure solution with separate deploy and src directories

---
+++ Orderly.UsersService/src/Orderly.Users.API/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using Orderly.Users.API.Middlewares;
+using Orderly.Users.Application.Data;
+
+namespace Orderly.Users.API.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+
+    public static void UseCustomExceptionHandler(this IApplicationBuilder builder)
+    {
+        builder.UseMiddleware<ExceptionHandlingMiddleware>();
+    }
+
+    public static async Task CreateDatabaseAsync(this IApplicationBuilder builder)
+    {
+        const string createUsersTable = @"
+        CREATE TABLE IF NOT EXISTS public.users (
+            id UUID PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            gender INT NOT NULL
+        );";
+
+        const int maxRetries = 5;
+        var retryDelay = TimeSpan.FromSeconds(2);
+        int attempt = 0;
+        bool success = false;
+
+        var connectionFactory = builder.ApplicationServices.GetRequiredService<ISqlConnectionFactory>();
+
+        do
+        {
+            try
+            {
+                using var connection = connectionFactory.CreateConnection();
+                await connection.ExecuteAsync(createUsersTable);
+                success = true;
+            }
+            catch (Exception)
+            {
+                attempt++;
+                if (attempt >= maxRetries)
+                    throw;
+
+                Console.WriteLine($"[DB Initialization] Attempt {attempt} failed. Retrying in {retryDelay.TotalSeconds} seconds...");
+                await Task.Delay(retryDelay);
+            }
+        } while (!success);
+    }
+
+}
--- Orderly.UsersService/src/Orderly.Users.API/Program.cs
+++ Orderly.UsersService/src/Orderly.Users.API/Program.cs
@@ -29,6 +29,8 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    await app.CreateDatabaseAsync();
 }
 
 app.UseCustomExceptionHandler();
